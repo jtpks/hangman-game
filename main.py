3024f2717b75a6d800a558c397982fd3093f58fc
jtpks <jahnavi.thejo@gmail.com>
2024-01-04 06:24:00 +0000

    Finished Hangman Game

--- main.py
+++ main.py
@@ -1,54 +1,135 @@
 import pygame
-import os
+import math
+import random
 
 #set up display
 
-pygame.init() #initializing pygames
-WIDTH, HEIGHT = 800, 500 #constants represented in all caps
-win = pygame.display.set_mode((WIDTH, HEIGHT)) #pygame accepts a tuple for width and height
-pygame.display.set_caption("Hangman Game!") #sets the caption (label on top)
+pygame.init()  #initializing pygames
+WIDTH, HEIGHT = 800, 500  #constants represented in all caps
+win = pygame.display.set_mode(
+    (WIDTH, HEIGHT))  #pygame accepts a tuple for width and height
+pygame.display.set_caption("Hangman Game!")  #sets the caption (label on top)
 
 #button variables
 RADIUS = 20
 GAP = 15
-LETTERS = []
-startx = round((WIDTH - (RADIUS * 2 + GAP)*13)/2 )
+letters = []  #letter now stores [34, 56, "A", True] for example
+startx = round((WIDTH - (RADIUS * 2 + GAP) * 13) / 2)
 starty = 400
-for i in range(26): #i tells which button we are on 
+A = 65
+for i in range(26):  #i tells which button we are on
   x = startx + GAP * 2 + ((RADIUS * 2 + GAP) * (i % 13))
-  y = starty + ((i // 13) * (GAP + RADIUS * 2)) # // = integer division, just excludes the division point 
-  
+  y = starty + ((i // 13) * (GAP + RADIUS * 2)
+                )  # // = integer division, just excludes the division point
+  letters.append(
+      [x, y, chr(A + i),
+       True])  #going to take the character representation of the integer A + i
 
-#load images 
+#fonts
+LETTER_FONT = pygame.font.SysFont("comicsans", 40)
+WORD_FONT = pygame.font.SysFont("comicsans", 60)
+TITLE_FONT = pygame.font.SysFont("comicsans", 70)
+
+#load images
 images = []
-for i in range(7): 
+for i in range(7):
   image = pygame.image.load("hangman" + str(i) + ".png")
   images.append(image)
 
 #game variables
-hangman_status = 4
-
+hangman_status = 0
+words = ["IDE", "REPLIT", "PYTHON", "PYGAME"]
+word = random.choice(words)  #needs to be all caps
+guessed = []  #list representing what letters we already guessed so far
 
 #colors
-WHITE = (255,255,255)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
 #set game loop
 
-FPS = 60 #frames per second
-clock = pygame.time.Clock() #clock object that counts at 60 FPS 
+FPS = 60  #frames per second
+clock = pygame.time.Clock()  #clock object that counts at 60 FPS
 run = True
 
-while (run):
-  clock.tick(FPS) #ticks the clock at 60 FPS
+
+def draw():
   win.fill(WHITE)
-  win.blit(images[hangman_status], (150, 100)) #stands for draw image & the POS of the image
-  pygame.display.update() #need to update the display to see the changes
-  
-  for event in pygame.event.get(): #look for any event that happens ex. user clicking the screen with their mouse
-    if event.type == pygame.QUIT: #if the user clicks the X on the window
+  #draw title
+  text = TITLE_FONT.render("DEVELOPER HANGMAN", 1, BLACK)
+  win.blit(text, (WIDTH / 2 - text.get_width(), 20))
+
+  #draw word
+  display_word = ""
+  for letter in word:
+    if letter in guessed:
+      display_word += letter + " "
+    else:
+      display_word += "_ "
+
+  text = WORD_FONT.render(display_word, 1, BLACK)
+  win.blit(text, (400, 200))
+
+  #draw buttons
+  for letter in letters:
+    x, y, ltr, visible = letter  #ltr stands for letter
+    if visible:  #only draw button if not clicked
+      pygame.draw.circle(win, BLACK, (x, y), RADIUS, 3)
+      text = LETTER_FONT.render(ltr, 1, BLACK)  #anti alias always kept to 1
+      win.blit(text, (x - (text.get_width()) / 2, y - (text.get_height()) / 2))
+
+  win.blit(images[hangman_status],
+           (150, 100))  #stands for draw image & the POS of the image
+  pygame.display.update()  #need to update the display to see the changes
+
+
+def display_message(message):
+  pygame.time.delay(1000)
+  win.fill(WHITE)
+  text = WORD_FONT.render(message, 1, BLACK)
+  win.blit(
+      text,
+      (WIDTH / 2 - text.get_width() / 2, HEIGHT / 2 - text.get_height() / 2))
+  pygame.display.update()
+  pygame.time.delay(
+      3000)  #delays for 3 seconds, text on screen will appear for 3 seconds
+
+
+while (run):
+  clock.tick(FPS)  #ticks the clock at 60 FPS
+  draw()
+  for event in pygame.event.get(
+  ):  #look for any event that happens ex. user clicking the screen with their mouse
+    if event.type == pygame.QUIT:  #if the user clicks the X on the window
       run = False
-    if event.type == pygame.MOUSEBUTTONDOWN:
-      pos = pygame.mouse.get_pos()
-      print(pos) #(0,0) coordinate is at the top left of the screen
+    if event.type == pygame.MOUSEBUTTONDOWN:  #checking for collision
+      m_x, m_y = pygame.mouse.get_pos()
+      for letter in letters:
+        x, y, ltr, visible = letter
+        if visible:
+          dis = math.sqrt((x - m_x)**2 + (y - m_y)**2)
+          if dis < RADIUS:
+            letter[
+                3] = False  #can't say visible is false since visible is a copy of the letter, but letter[3] stores the original copy of the boolean value
+            guessed.append(
+                ltr
+            )  #character of the letter we are looking at in the collision loop
+            if ltr not in word:
+              hangman_status += 1
+
+  draw()
+  won = True
+  for letter in word:
+    if letter not in guessed:
+      won = False
+      break
+
+  if won:
+    display_message("YOU WON!")
+    break
+
+  if hangman_status == 6:
+    display_message("YOU LOST!")
+    break
 
 pygame.quit()
